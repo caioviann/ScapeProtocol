d303cc78a5aca1137719242d57c1987a8b278515
caio henrique <caiohenriqueviana01@gmail.com>
2026-06-25 09:46:44 -0300

    add img background menu, game over and victory

--- ScapeControlCode/src/game/scenes/GameOver.ts
+++ ScapeControlCode/src/game/scenes/GameOver.ts
@@ -4,7 +4,6 @@ export class GameOver extends Scene
 {
     camera: Phaser.Cameras.Scene2D.Camera;
     background: Phaser.GameObjects.Image;
-    gameover_text : Phaser.GameObjects.Text;
     restart_text: Phaser.GameObjects.Text;
     private playerNumber = 1;
     private won = false;
@@ -27,17 +26,11 @@ export class GameOver extends Scene
         this.camera = this.cameras.main
         this.camera.setBackgroundColor(this.won ? 0x0f766e : 0xff0000);
 
-        this.background = this.add.image(512, 384, 'background');
-        this.background.setAlpha(0.5);
+        this.background = this.add.image(512, 384, this.won ? 'victoryBackground' : 'gameOverBackground');
+        this.background.setDisplaySize(1024, 768);
 
-        this.gameover_text = this.add.text(512, 384, this.won ? 'Voce venceu!\nConseguiu escapar.' : 'Game Over', {
-            fontFamily: 'Arial Black', fontSize: this.won ? 54 : 64, color: '#ffffff',
-            stroke: '#000000', strokeThickness: 8,
-            align: 'center'
-        });
-        this.gameover_text.setOrigin(0.5);
-
-        this.restart_text = this.add.text(512, 500, 'Pressione X no controle ou Enter no teclado para recomecar', {
+        this.add.rectangle(512, 704, 1024, 88, 0x000000, 0.55);
+        this.restart_text = this.add.text(512, 704, 'Pressione X no controle ou Enter no teclado para recomecar', {
             fontFamily: 'Arial',
             fontSize: 24,
             color: '#ffffff',
